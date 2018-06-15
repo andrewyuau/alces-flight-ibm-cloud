2bec1f06e4cc1fad4333409205dee61c96f7f618
Andrew Yu <andrewyu.au@gmail.com>
2018-06-15 23:05:38 +1000

    Create compute-solo-bare-metal.sh

---
+++ compute-solo-bare-metal.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://api.service.softlayer.com/rest/v3.1/SoftLayer_Resource_Metadata/getUserMetadata | sed -e "s/\\\//g" | sed -e "s/\"#/#/g" | sed -e "s/rn\"/\n/g" | sed -e "s/rn/\n/g" >> userData.sh
+chmod 755 userData.sh
+./userData.sh
