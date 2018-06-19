cda94b19a0e61fb4ef7a1ce668d83048401ffbce
Andrew Yu <andrewyu.au@gmail.com>
2018-06-19 11:48:20 +1000

    Update compute-solo-bare-metal.sh

--- compute-solo-bare-metal.sh
+++ compute-solo-bare-metal.sh
@@ -4,4 +4,4 @@ cat <<EOF > /root/userData.sh
 EOF
 chmod 755 /root/userData.sh
 echo "$(curl -w "\n" https://api.service.softlayer.com/rest/v3.1/SoftLayer_Resource_Metadata/getUserMetadata | sed -e "s/\"wget/wget/g"  | sed -e "s/\\\n\"//g" | sed -e "s/\\\//g")" >> /root/userData.sh
-/bin/bash /root/userData.sh
+/bin/bash -c '/root/userData.sh'
